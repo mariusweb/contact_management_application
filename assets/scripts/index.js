1ffee28c8788651f0a92da92faea8076e158b996
mariusweb <39734577+mariusweb@users.noreply.github.com>
2020-07-10 23:16:02 +0300

    Added delete button

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,5 +1,16 @@
 "use strict";
 
+const form = document.querySelector(".input-form");
+
+let firstNameInput = document.querySelector(".first-name");
+let lastNameInput = document.querySelector(".last-name");
+let dateOfBirthInput = document.querySelector(".birthdate");
+let phoneNumberInput = document.querySelector(".phone-number");
+let emailInput = document.querySelector(".e-mail");
+let addressInput = document.querySelector(".address");
+
+const container = document.querySelector(".container");
+
 // Fetching data from git reposetory
 async function getData() {
   let response = await fetch(
@@ -11,23 +22,23 @@ async function getData() {
 
 // Adding data from JSON file to localstorage
 getData().then((data) => {
-  localStorage.setItem("users", JSON.stringify(data));
+  function noop() {}
+  data.map((user) => {
+    function foo() {
+      foo = noop();
+      let id = Math.random().toString(16).slice(2);
+      user.id = id;
+      localStorage.setItem("users", JSON.stringify(data));
+    }
+    // foo();
+  });
 });
-let users = JSON.parse(localStorage.getItem("users"));
 
 // Adding input values in to localstorage
-const form = document.querySelector("form");
-const submitButton = document.querySelector(".submit-button");
-
-const firstNameInput = document.querySelector(".first-name");
-const lastNameInput = document.querySelector(".last-name");
-const dateOfBirthInput = document.querySelector(".birthdate");
-const phoneNumberInput = document.querySelector(".phone-number");
-const emailInput = document.querySelector(".e-mail");
-const addressInput = document.querySelector(".address");
-
+const users = JSON.parse(localStorage.getItem("users"));
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  let usersNew = JSON.parse(localStorage.getItem("users")) || [];
 
   let user = {
     firstName: firstNameInput.value,
@@ -36,47 +47,112 @@ form.addEventListener("submit", (event) => {
     phoneNumber: phoneNumberInput.value,
     email: emailInput.value,
     address: addressInput.value,
+    id: Math.random().toString(16).slice(2),
   };
-  let usersn = [];
-  usersn.push(user);
-  //   form.reset();
-  localStorage.setItem("usersn", JSON.stringify(usersn));
+  let userRepite = {
+    phoneNumber: phoneNumberInput.value,
+    email: emailInput.value,
+  };
+
+  if (
+    (user.firstName &&
+      user.lastName &&
+      user.dateOfBirth &&
+      user.phoneNumber &&
+      user.email &&
+      user.address &&
+      user.id) ||
+    (user.firstName &&
+      user.lastName &&
+      user.dateOfBirth &&
+      user.phoneNumber &&
+      user.email &&
+      user.id)
+  ) {
+    usersNew.push(user);
+    localStorage.setItem("users", JSON.stringify(usersNew));
+  }
   location.reload();
 });
-
-console.log(users);
-// localStorage.setItem("users", JSON.stringify(users));
+const usersUpdate = JSON.parse(localStorage.getItem("users"));
+console.log(usersUpdate);
 
 // Adding users to html file
-const container = document.querySelector(".container");
-let table = document.createElement("table");
+
+const table = document.createElement("table");
 table.classList.add("users-list");
 container.appendChild(table);
 
-users.map((user) => {
-  let tr = document.createElement("tr");
-  tr.classList.add("output");
+const addingUser = () => {
+  let usersNew = JSON.parse(localStorage.getItem("users"));
+  usersNew.map((user) => {
+    let tr = document.createElement("tr");
+    tr.classList.add("output" + user.id);
 
-  tr.innerHTML = `
-        <td>${user.firstName} ${user.lastName}</td>
-        <td>${user.dateOfBirth}</td>
-        <td>${user.phoneNumber}</td>
-        <td>${user.email}</td>
-        <td>${user.address}</td>
-    `;
-  table.appendChild(tr);
-});
-let usersNew = JSON.parse(localStorage.getItem("usersn"));
-usersNew.map((user) => {
-  let tr = document.createElement("tr");
-  tr.classList.add("output");
+    const buttonDelete = document.createElement("button");
+    buttonDelete.innerText = "Delete";
+    buttonDelete.classList.add("delete" + user.id);
 
-  tr.innerHTML = `
-          <td>${user.firstName} ${user.lastName}</td>
-          <td>${user.dateOfBirth}</td>
-          <td>${user.phoneNumber}</td>
-          <td>${user.email}</td>
-          <td>${user.address}</td>
+    const buttonEdit = document.createElement("button");
+    buttonEdit.innerText = "Edit";
+    buttonEdit.classList.add("edit" + user.id);
+
+    tr.innerHTML = `
+          
+          <td>First name: ${user.firstName}</td>
+          <td>Last name: ${user.lastName}</td>
+          <td>Date of birth: ${user.dateOfBirth}</td>
+          <td>Phone number: ${user.phoneNumber}</td>
+          <td>E-mail: ${user.email}</td>
+          <td>Address: ${user.address}</td>
+          
       `;
-  table.appendChild(tr);
-});
+
+    tr.prepend(buttonEdit);
+    tr.appendChild(buttonDelete);
+
+    table.appendChild(tr);
+  });
+
+  // Deleting user
+
+  usersNew.map((user) => {
+    let usersDelete = JSON.parse(localStorage.getItem("users"));
+    const deleteButton = document.querySelector(".delete" + user.id);
+
+    deleteButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      usersDelete = usersDelete.filter((userDel) => {
+        return userDel.id !== user.id;
+      });
+      localStorage.setItem("users", JSON.stringify(usersDelete));
+      location.reload();
+    });
+
+    let usersEdit = JSON.parse(localStorage.getItem("users"));
+    const buttonEdit = document.querySelector(".edit" + user.id);
+
+    buttonEdit.addEventListener("click", (event) => {
+      event.preventDefault();
+      let editForm = document.createElement("form");
+      editForm.classList.add("edit-form" + user.id);
+      let output = document.querySelector(".output" + user.id);
+      console.log(output);
+
+      editForm.innerHTML = `
+      <input type="submit" value="Save">
+        <input type="text" required value="${user.firstName}">
+        <input type="text" required value="${user.lastName}">
+        <input type="date" required value="${user.dateOfBirth}">
+        <input type="tel" required value="${user.phoneNumber}">
+        <input type="email" required value="${user.email}">
+        <input type="text" value="${user.address}">
+      `;
+      output = editForm;
+      console.log(editForm);
+      // usersEdit.map((userEdit) => {});
+      // location.reload();
+    });
+  });
+};
+addingUser();
